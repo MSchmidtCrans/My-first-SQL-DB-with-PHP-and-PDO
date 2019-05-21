e99b00a9bd79c01f0702dd652dcbad8547bc8e28
supersmitty2018 <48794241+supersmitty2018@users.noreply.github.com>
2019-05-21 14:34:07 +0200

    php-mysql re-install was needed before succes

--- myPhp.php
+++ myPhp.php
@@ -3,14 +3,20 @@ $servername = "localhost";
 $username = "phpdb";
 $password = "wachtwoord";
 
+
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    $conn = new PDO("mysql:host=$servername", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
+    $sql = "CREATE DATABASE myDBPDO";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "Database created successfully<br>";
     }
 catch(PDOException $e)
     {
-    echo "Connection failed: " . $e->getMessage();
+    echo $sql . "<br>" . $e->getMessage();
     }
+
+$conn = null;
 ?>
