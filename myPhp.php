4ffdd8ba362fc088c5c6471b138d1f84cb18a765
supersmitty2018 <48794241+supersmitty2018@users.noreply.github.com>
2019-05-21 14:45:33 +0200

    Created table in database with php

--- myPhp.php
+++ myPhp.php
@@ -5,13 +5,22 @@ $password = "wachtwoord";
 
 
 try {
-    $conn = new PDO("mysql:host=$servername", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE DATABASE myDBPDO";
+    
+    // sql to create table
+    $sql = "CREATE TABLE adressCards (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+        firstname VARCHAR(30) NOT NULL,
+        lastname VARCHAR(30) NOT NULL,
+        gender VARCHAR(5),
+        city VARCHAR(30)
+        )";
+
     // use exec() because no results are returned
     $conn->exec($sql);
-    echo "Database created successfully<br>";
+    echo "Table MyGuests created successfully";
     }
 catch(PDOException $e)
     {
