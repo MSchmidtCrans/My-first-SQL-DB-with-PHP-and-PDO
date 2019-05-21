2f1f0fb39885e06ef434e6d24f6268b8ccde96ac
supersmitty2018 <48794241+supersmitty2018@users.noreply.github.com>
2019-05-21 14:53:29 +0200

    Inserted data into db table

--- myPhp.php
+++ myPhp.php
@@ -9,18 +9,12 @@ try {
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    // sql to create table
-    $sql = "CREATE TABLE adressCards (
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-        firstname VARCHAR(30) NOT NULL,
-        lastname VARCHAR(30) NOT NULL,
-        gender VARCHAR(5),
-        city VARCHAR(30)
-        )";
+$sql = "INSERT INTO adressCards (firstname, lastname, gender, city)
+VALUES ('Michael', 'Schmidt Crans', 'man', 'Leeuwarden')";    
 
     // use exec() because no results are returned
     $conn->exec($sql);
-    echo "Table MyGuests created successfully";
+    echo "Records created successfully";
     }
 catch(PDOException $e)
     {
