6d9a159c04cd38f7a9450aa64ba0b3b2ae4f9e27
supersmitty2018 <48794241+supersmitty2018@users.noreply.github.com>
2019-05-22 11:42:59 +0200

    Array created and comment with steps added

---
+++ myPHP_JSON.php
@@ -0,0 +1,41 @@
+<?php
+
+header('Content-type: application/json; charset=utf-8');
+
+$servername = "localhost";
+$username = "phpdb";
+$password = "wachtwoord";
+
+
+try {
+    //connect to DB
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    //Prepare and execute mysql query
+    $itemsquery = $conn->prepare("SELECT * FROM adressCards");
+    $itemsquery->execute();
+
+    //Set array
+    $persons = array();
+    
+    //Loop through all rows from table
+    foreach($itemsquery as $item) {
+        echo($item['firstname'].' '.$item['lastname'].' '.$item['gender'].' '.$item['city'].'</br>');    
+
+    //Add value to array    
+    }
+
+    //Sent array as JSON
+    echo json_encode($persons);
+
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
+?>
