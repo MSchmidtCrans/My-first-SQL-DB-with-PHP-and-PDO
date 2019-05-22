69d40e6d028a9efc50d2d5c2d50da68af55b0a69
supersmitty2018 <48794241+supersmitty2018@users.noreply.github.com>
2019-05-22 09:51:54 +0200

    Read data from database

--- myPhp.php
+++ myPhp.php
@@ -8,14 +8,16 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-$sql = "INSERT INTO adressCards (firstname, lastname, gender, city)
-VALUES ('Michael', 'Schmidt Crans', 'man', 'Leeuwarden')";    
+
+
+$sql = "SELECT * FROM adressCards";    
 
     // use exec() because no results are returned
-    $conn->exec($sql);
-    echo "Records created successfully";
+    
+    echo "Records selected successfully";
+    
     }
+
 catch(PDOException $e)
     {
     echo $sql . "<br>" . $e->getMessage();
